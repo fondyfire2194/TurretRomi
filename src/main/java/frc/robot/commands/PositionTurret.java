95ab4bb0348003418e6aaeb05e75669233b96028
icemannie <boyle.john65@gmail.com>
2021-03-31 07:45:21 -0500

    Added tuning for position

--- src/main/java/frc/robot/commands/PositionTurret.java
+++ src/main/java/frc/robot/commands/PositionTurret.java
@@ -4,7 +4,9 @@
 
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.controller.PIDController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Turret;
 
@@ -14,32 +16,46 @@ public class PositionTurret extends CommandBase {
   private final Turret m_turret;
   private double m_position;
 
-  private PIDController m_controller = new PIDController(0.003, 0, 0);
+  private double kp = .003;
+  private double ki = 0;
+  private double kd = 0;
+  private double startTime;
+  private double iZone = 10;
+
+  private PIDController m_controller = new PIDController(kp, 0, 0);
 
   public PositionTurret(Turret turret, double position) {
     // Use addRequirements() here to declare subsystem dependencies.
     m_turret = turret;
     m_position = position;
-  
+
     addRequirements(m_turret);
+
+    SmartDashboard.putNumber("Kp", kp);
+    SmartDashboard.putNumber("Ki", ki);
+    SmartDashboard.putNumber("Kd", kd);
+    m_controller.setIntegratorRange(0, .01);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
- 
+    startTime = Timer.getFPGATimestamp();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-  
-    
-    double output = m_controller.calculate(m_turret.getTurretPosition(), m_position);
-    
+    kp = SmartDashboard.getNumber("Kp", kp);
+    ki = SmartDashboard.getNumber("Ki", ki);
+    kd = SmartDashboard.getNumber("Kd", kd);
+    m_controller.setPID(kp, 0, kd);
+    if (Math.abs(m_position - m_turret.getTurretPosition()) < iZone) {
+      m_controller.setI(ki);
+    }
 
-   
+    double output = m_controller.calculate(m_turret.getTurretPosition(), m_position);
+
     m_turret.turnTurret(output);
   }
 
@@ -47,11 +63,13 @@ public class PositionTurret extends CommandBase {
   @Override
   public void end(boolean interrupted) {
     m_turret.turnTurret(0);
+   m_controller.reset();
   }
 
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return Math.abs(m_position - m_turret.getTurretPosition()) < 2 || m_turret.onCCWLimit() || m_turret.onCWLimit();
+    return Math.abs(m_position - m_turret.getTurretPosition()) < 2 || m_turret.onCCWLimit() || m_turret.onCWLimit()
+        || Timer.getFPGATimestamp() > startTime + 2;
   }
 }
